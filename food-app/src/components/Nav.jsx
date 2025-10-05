8bffa77eb98479eeb62650bc2fea7942531e8c0a
dipangshu22 <dipangshu22@gmail.com>
2025-10-05 12:16:49 +0530

    Initial commit from project folder

---
+++ food-app/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+   <>
+    <div className='w-full h-[100px] bg-blue-900'></div>
+   </>
+  )
+}
---
+++ food-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+export default function Home() {
+  return (
+    <div className='w-full min-h-screen bg-amber-100'>
+    
+    <Nav></Nav></div>
+    
+        
+    
+  )
+}
