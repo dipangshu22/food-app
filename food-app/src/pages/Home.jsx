75033d30afbb22ef90581a4d42791e3b994840b1
dipangshu22 <dipangshu22@gmail.com>
2025-10-06 12:05:19 +0530

    filter

--- food-app/src/pages/Home.jsx
+++ food-app/src/pages/Home.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Nav from '../components/Nav'
 import Category from '../Category'
 import Card from '../components/Card'
 import { food_items } from '../food'
 
-export default function Home() {
+export default function  Home() {
+  const [part,setPart]=useState(food_items)
+
+   const filter=(category)=>{
+    if(category==="All"){
+      setPart(food_items)}
+      else{
+        let newlist=food_items.filter((item)=>item.food_category===category
+        )
+        setPart(newlist)
+      }
+  }
   return (
     <div className='w-full min-h-screen bg-amber-100'>
     
     <Nav></Nav>
-    <Category></Category>
-    <div className='w-full flex flex-wrap gap-3 p-3 justify-center items-center'>{food_items.map((items)=>{
-      return <Card name={items.food_name} food_type={items.food_type} food_image={items.food_image} food_price={items.price} ></Card>
+    <Category catg={filter}></Category>
+    <div className='w-full flex flex-wrap gap-3 pt-5 pb-8 justify-center items-center'>{part.map((items)=>{
+      return <Card name={items.food_name} food_type={items.food_type} food_image={items.food_image} food_price={items.price}   ></Card>
 
     })}</div>
     
@@ -21,3 +32,4 @@ export default function Home() {
     
   )
 }
+ 
