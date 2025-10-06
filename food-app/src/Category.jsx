483e44c4e732dee238a85cd2ff3ec362338efdc7
dipangshu22 <dipangshu22@gmail.com>
2025-10-06 12:07:27 +0530

    filter1

--- food-app/src/Category.jsx
+++ food-app/src/Category.jsx
@@ -32,7 +32,7 @@ export default function Category(props) {
         },
         {
             id:5,
-            name:"main_course",
+            name:"main course",
             image:<FaBowlFood  className='w-[40px] h-[40px]  text-green-300'/>
         },
         {
